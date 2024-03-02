45a76b2dd45d6c0dbe53c8cb874f37c524916631
Densell <dguevaram4@miumg.edu.gt>
2024-03-02 00:29:19 -0600

    commit 2

--- main.cpp
+++ main.cpp
@@ -34,6 +34,94 @@ int main() {
 
 
 
+    // Bucle principal
+    while (true) {
+        // Cadena de entrada
+        string input;
+        cout << "Ingrese expresiones (o 'exit' para salir): ";
+        getline(cin, input);
+
+        // Salir del bucle si se ingresa la palabra "exit"
+        if (input == "exit") {
+            break;
+        }
+
+        // Almacenar lexemas en un vector
+        vector<Lexema> lexemas;
+
+        // Procesar cada carácter en la entrada
+        for (char caracter : input) {
+            Lexema lexema;
+            lexema.token = caracter;
+
+            // Comprobar si el carácter es una palabra clave
+            bool isKeyword = false;
+            for (const string& keyword : keywords) {
+                if (caracter == keyword[0]) {
+                    lexema.tipo = "Keyword";
+                    isKeyword = true;
+                    break;
+                }
+            }
+
+            if (!isKeyword) {
+                // Comprobar si el carácter es un operador especial
+                for (const string& operador : operadoresDeComparacion) {
+                    if (caracter == operador[0]) {
+                        lexema.tipo = "Operador especial";
+                        isKeyword = true;
+                        break;
+                    }
+                }
+            }
+
+
+            if (!isKeyword) {
+                // Comprobar si el carácter coincide con alguna palabra
+                for (const string& palabra : palabrasEspecificas) {
+                    if (input.find(palabra) != string::npos) {
+                        lexema.tipo = "Palabra";
+                        isKeyword = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isKeyword) {
+                // Comprobar si el carácter es un operador aritmetico
+                for (const string& Operador : OperadorAritmetico) {
+                    if (caracter == Operador[0]) {
+                        lexema.tipo = "Operador aritmetico";
+                        isKeyword = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isKeyword) {
+                // Comprobar si el carácter coincide con alguna expresión regular
+                if (regex_match(string(1, caracter), integerRegex)) {
+                    lexema.tipo = "Numero";
+                } else if (regex_match(string(1, caracter), wordRegex)) {
+                    if (isupper(caracter)) {
+                        lexema.tipo = "Letra mayuscula";
+                    } else if (islower(caracter)) {
+                        lexema.tipo = "Letra minuscula";
+                    }
+                } else if (regex_match(std::string(1, caracter), symbolRegex)) {
+                    lexema.tipo = "Simbolo";
+                } else {
+                    lexema.tipo = "Desconocido";
+                }
+            }
+
+            // Agregar lexema al vector
+            lexemas.push_back(lexema);
+        }
+
+        // Mostrar el resultado de manera vertical
+        for (const auto& lexema : lexemas) {
+            cout << lexema.token << "=" << lexema.tipo << "\n";
         }
     }
 
